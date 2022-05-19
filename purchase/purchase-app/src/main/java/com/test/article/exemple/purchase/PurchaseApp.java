d3af6b3432f5f3a36befbcf6f1cb19c0d6afba85
Kevin DUPEYRAT <k.dupeyrat@groupeonepoint.com>
2022-05-19 22:07:03 +0200

    Initialisation

---
+++ purchase/purchase-app/src/main/java/com/test/article/exemple/purchase/PurchaseApp.java
@@ -0,0 +1,13 @@
+package com.test.article.exemple.purchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurchaseApp.class, args);
+    }
+
+}
